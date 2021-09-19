ef44f75b5db42c141348489ec9a389a7c73ca9b6
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-09-19 10:08:35 +0100

    Implement categories section

---
+++ src/components/Products.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+`;
+
+const Products = () => {
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
+
+export default Products;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -84,7 +84,7 @@ const Slider = () => {
     <Container>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} key={item.id}>
             <ImgContainer>
               <Image src={item.img} />
             </ImgContainer>
