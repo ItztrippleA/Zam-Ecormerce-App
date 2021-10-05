d092cb442eb456eeede92fdb2ee9e7da978a82cf
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-05 01:39:12 +0100

    Title prop

--- src/pages/Home/ProductList.jsx
+++ src/pages/Home/ProductList.jsx
@@ -55,7 +55,7 @@ const ProductList = () => {
     <Container>
       <Navbar />
       <Announcement />
-      <Title>Dresses</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
